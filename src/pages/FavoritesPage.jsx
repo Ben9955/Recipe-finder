f5d0b721060abf46a0b0682b5cc5d15de43c76d6
ben9955 <ben95ansari@gmail.com>
2024-07-05 14:46:54 +0100

    Created the favorites page which will render a 404 page in case no favorites are present

--- src/pages/FavoritesPage.jsx
+++ src/pages/FavoritesPage.jsx
@@ -1,25 +1,33 @@
-import { Search } from "lucide-react";
+import RecipeCard from "../components/RecipeCard";
 
 const FavoritesPage = () => {
+  const fav = true;
   return (
-    <div className="bg-[#faf9fb] p-10 flex-1">
+    <div className="bg-[#faf9fb] flex-1 p-10 min-h-screen ">
       <div className="max-w-screen-lg mx-auto">
-        <form className="">
-          <label htmlFor="" className="input shadow-md flex items-center gap-2">
-            <Search size={"24"} />
-            <input
-              type="text"
-              className="text-sm md:textmd grow"
-              placeholder="What do you want to cook today?"
-            />
-          </label>
-        </form>
-        <p className="font-bold text-3xl md:text-5xl mt-4 ">
-          Recommended Recipes
+        <p className="font-bold capitalize text-3xl md:text-5xl my-4">
+          my favorites
         </p>
-        <p className="text-slate-500 font-semibold ml-1 my-2 text-sm tracking-tight ">
-          Popular choices
-        </p>
+
+        {!fav && (
+          <div className="h-[80vh] flex flex-col items-center gap-4">
+            <img src="/404.svg" className="h-3/4" alt="404 svg" />
+          </div>
+        )}
+
+        {fav && (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 ">
+            <RecipeCard />
+            <RecipeCard />
+            <RecipeCard />
+            <RecipeCard />
+            <RecipeCard />
+            <RecipeCard />
+            <RecipeCard />
+            <RecipeCard />
+            <RecipeCard />
+          </div>
+        )}
       </div>
     </div>
   );
